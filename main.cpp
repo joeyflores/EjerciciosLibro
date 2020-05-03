8f7377b02e4af86f59b5095738a3c0534af8fbe5
joeyflores <54363951+joeyflores@users.noreply.github.com>
2020-05-02 20:03:52 -0500

    ertyu30

--- main.cpp
+++ main.cpp
@@ -4,21 +4,15 @@ using namespace std;
 
 int main()
 {
-    int n1;
-    int x,y=0,z;
-    cout<<"Ingrese su numero: ";cin>>n1;//121
-    z=n1;
-
-    while(n1!=0){
-        x=n1%10;            //121%10=1  12%10=2         1%10=1
-        y=(y*10)+x;         //0+1=1     (1*10)+2=12     (12*10)+1=121
-        n1=n1/10;           //121/10=12  12/10=1        1/10=0
-    }
-    if(y==z){
-        cout<<"si es palindrome"<<endl;
-    }
-    else{
-        cout<<"no es palindrome"<<endl;
+    int a=0,b=1,temp;
+    while(true){
+        temp=b;
+        b=a+b,a=temp;
+        cout<<b<<endl;
     }
     return 0;
 }
+/*
+Como la condicion es True el bucle es infinito,ademas hagarra valores negativos porque
+se pasa de la cantidad de bits que usa el tipo de valor int.
+*/
