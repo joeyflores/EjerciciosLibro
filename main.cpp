97c07c12218e3e9ef365a8ebad35e550800f7f24
joeyflores <54363951+joeyflores@users.noreply.github.com>
2020-05-02 19:58:11 -0500

    EJERCICIO 4.27

--- main.cpp
+++ main.cpp
@@ -5,22 +5,20 @@ using namespace std;
 int main()
 {
     int n1;
-    cout<<"Ingrese la longitud de su cuadrado: ";cin>>n1;
+    int x,y=0,z;
+    cout<<"Ingrese su numero: ";cin>>n1;//121
+    z=n1;
 
-
-    for(int i=1;i<=n1;i++){
-
-        for(int j=1;j<=n1;j++){
-            if(i==1||i==n1||j==1||j==n1){
-                cout<<"*";
-            }
-            else{
-                cout<<" ";
-            }
-        }
-        cout<<endl;
+    while(n1!=0){
+        x=n1%10;            //121%10=1  12%10=2         1%10=1
+        y=(y*10)+x;         //0+1=1     (1*10)+2=12     (12*10)+1=121
+        n1=n1/10;           //121/10=12  12/10=1        1/10=0
     }
-
-
+    if(y==z){
+        cout<<"si es palindrome"<<endl;
+    }
+    else{
+        cout<<"no es palindrome"<<endl;
+    }
     return 0;
 }
